b18916074142a0c9ba6b24f668a1bca9dff3beda
河本 勇人 <kawamoto.web@gmail.com>
2022-10-25 23:55:34 +0900

    ちょっと修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import { useState } from 'react'
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 
-function App() {
-
-  const [todoText,setTodoText] = useState("")
-  const [todoList,setTodoList] = useState([
+function App () {
+  const [todoList, setTodoList] = useState([
     {
-      id:uuid(),
-      text:"",
+      id: uuid(),
+      text: '',
       isDone: false,
     },
   ])
 
-  const [checkFlag,setCheckFlag] = useState(true)
+  const onChangeTodoText = (id, text) => {
+    setTodoList(todoList.map((todo) => ({
+      ...todo,
+      text: todo.id === id ? text : todo.text
+    })))
+  }
 
-  const onChangeTodoText = (event) => setTodoText(event.target.value)
-
-  const handleRemoveTask = (id,index,todo) => {
-    const listNum = todoList.length
-    if(listNum === 1){
-      return setTodoList(todoList.map((todo)=>{
-        alert(todo.isDone)
-        return{
-          ...todo,
-          text: "",
-        }
-      }))
-      // setTodoList(todoList.map((todo) => {}delete todo.text))
-
+  const handleRemoveTask = (id) => {
+    if (todoList.length === 1) {
+      return setTodoList([
+        {
+          id: uuid(),
+          text: '',
+          isDone: false,
+        },
+      ])
     }
-    if(index === 0) return
-    setTodoList(todoList.filter((todo)=>todo.id !== id))
+    setTodoList(todoList.filter((todo) => todo.id !== id))
   }
 
   const toggleTaskCheck = (id) => {
-   setTodoList(todoList.map((todo)=>{
-     let isDone
-     if(todo.id === id){
-       isDone = !todo.isDone
-     }else{
-       isDone = todo.isDone
-     }
-     return{
-       ...todo,
-       isDone: isDone,
-     }
-   }))
+    setTodoList(todoList.map((todo) => {
+      return {
+        ...todo,
+        isDone: todo.id === id ? !todo.isDone : todo.isDone,
+      }
+    }))
   }
 
-  const handleAddTask = (e)=> {
-   if (e.key == 'Enter') {
-     if(todoText==="")return
-     setTodoList([
-       ...todoList,
-       {
-         id:uuid(),
-         text:todoText,
-       }
-     ])
-     setTodoText("")
+  const handleAddTask = (e) => {
+    if (e.key === 'Enter') {
+      setTodoList([
+        ...todoList,
+        {
+          id: uuid(),
+          text: '',
+          isDone: false,
+        }
+      ])
     }
-   }
+  }
 
   return (
     <div>
-    <div>
-      <ul>
-        {todoList.map((todo,index)=>{
-          return(
-        <div key={todo.id}>
-          {todo.isDone && <button className='CheckOff-button' onClick={()=>toggleTaskCheck(todo.id)}>✔</button>}
-          {todo.isDone || <button className='check-button' onClick={()=>toggleTaskCheck(todo.id)}></button>}
-          <input className={todo.isDone ? "completeTodo" : "incompleteTodo"}
-                 value={todoList.index}
-                 onChange={onChangeTodoText}
-                 placeholder="やることを入力してください"
-                 onKeyPress={handleAddTask}
-          />
-          <button onClick={() => handleRemoveTask(todo,todo.id,index)}>
-            del
-          </button>
-        </div>
-          )
-        })}
-      </ul>
-    </div>
+      <div>
+        <ul>
+          {todoList.map((todo, index) => {
+            return (
+              <div key={todo.id}>
+                <button
+                  className={todo.isDone ? 'CheckOff-button' : 'check-button'}
+                  onClick={() => toggleTaskCheck(todo.id)}
+                >{todo.isDone && '✔'}</button>
+                <input
+                  className={todo.isDone ? 'completeTodo' : 'incompleteTodo'}
+                  value={todo.text}
+                  onChange={(e) => onChangeTodoText(todo.id, e.target.value)}
+                  placeholder="やることを入力してください"
+                  onKeyDown={handleAddTask}
+                />
+                <button onClick={() => handleRemoveTask(todo, todo.id, index)}>
+                  del
+                </button>
+              </div>
+            )
+          })}
+        </ul>
       </div>
-  );
+    </div>
+  )
 }
 
-
-export default App;
+export default App
 
